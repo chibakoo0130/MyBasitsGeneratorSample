f88c57f13e06c090e45d33df66a1f13560d388e5
chibakoo0130 <kohtarocb@gmail.com>
2025-03-28 23:33:40 +0900

    add MyBatis Generator Sample Code

---
+++ src/main/java/com/example/mybatisgeneratorsample/MybatisgeneratorsampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybatisgeneratorsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisgeneratorsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisgeneratorsampleApplication.class, args);
+	}
+
+}
